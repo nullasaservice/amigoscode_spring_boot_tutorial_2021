42f58d8980275896fc775eee69c0ca525ba163a7
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-16 13:28:40 +0100

    Chapter 12 "Dependency Injection" done

--- src/main/kotlin/com/example/demo/student/StudentsService.kt
+++ src/main/kotlin/com/example/demo/student/StudentsService.kt
@@ -1,8 +1,10 @@
 package com.example.demo.student
 
+import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.Month
 
+@Service
 class StudentService {
     fun getStudents() = listOf(Student(1, "Pepe", "pepe@example.com", LocalDate.of(2000, Month.JANUARY, 5), 24))
 }
