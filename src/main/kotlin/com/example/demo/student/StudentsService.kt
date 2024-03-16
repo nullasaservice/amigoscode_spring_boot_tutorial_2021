d57afda5f66b635a7be9a92076911e69ae2847b5
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-16 20:11:13 +0100

    Chapter 17 "JPA Repository" done

--- src/main/kotlin/com/example/demo/student/StudentsService.kt
+++ src/main/kotlin/com/example/demo/student/StudentsService.kt
@@ -1,10 +1,15 @@
 package com.example.demo.student
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.Month
 
 @Service
-class StudentService {
-    fun getStudents() = listOf(Student(1, "Pepe", "pepe@example.com", LocalDate.of(2000, Month.JANUARY, 5), 24))
+class StudentService(
+    @Autowired
+    private val studentRepository: StudentRepository
+) {
+    // Student(1, "Pepe", "pepe@example.com", LocalDate.of(2000, Month.JANUARY, 5), 24)
+    fun getStudents() = studentRepository.findAll()
 }
