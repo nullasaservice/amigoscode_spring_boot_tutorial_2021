b35479ae2f4e1c6014522e966395fe4db7b4a3bb
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-16 13:13:09 +0100

    Chapter 10 "API Layer" done

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,19 +1,10 @@
 package com.example.demo
 
-import com.example.demo.student.Student
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
-import java.time.Month
 
 @SpringBootApplication
-@RestController
-class DemoApplication {
-	@GetMapping
-	fun hello() = listOf(Student(1, "Pepe", "pepe@example.com", LocalDate.of(2000, Month.JANUARY, 5), 24))
-}
+class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
