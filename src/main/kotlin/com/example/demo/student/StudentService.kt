23a63a54539730ecfaa462ff64b7976dfcd5c750
Carlos Sánchez <36648629+nullasaservice@users.noreply.github.com>
2024-03-19 08:20:05 +0100

    Chapter 23 "Deleting Students" done

--- src/main/kotlin/com/example/demo/student/StudentService.kt
+++ src/main/kotlin/com/example/demo/student/StudentService.kt
@@ -1,7 +1,10 @@
 package com.example.demo.student
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import java.lang.IllegalStateException
 
 @Service
@@ -13,8 +16,15 @@ class StudentService(
     fun addNewStudent(student: Student) {
         var studentByEmail = studentRepository.findStudentByEmail(student.email)
 
-        if (studentByEmail.isPresent) throw IllegalStateException("Email is already taken")
+        if (studentByEmail.isPresent)
+            throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Email is already taken")
 
         studentRepository.save(student)
+    }
+    fun deleteStudent(studentId: Long) {
+        if (!studentRepository.existsById(studentId))
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Student not found")
+
+        studentRepository.deleteById(studentId)
     }
 }
